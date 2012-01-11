784c2271e4988620e97516123b5f73a8143917e6
Python Snake <pythonsnake98@gmail.com>
2012-01-11 11:31:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 #-*- coding: Latin-1 -*-
-#  Copyright 2012 Python Snake
+# Copyright 2012 Python Snake
+#  This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
 
  """A very simple Python bot.
  
